3a6e11a4cbc4b73c76ebf618d17df829c20b84ed
Shelixer02 <60590552+Shelixer02@users.noreply.github.com>
2020-02-10 19:18:06 +0700

    Add files via upload

---
+++ logic.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long choice;
+	long p,q;
+	cout<<"1. conjunction\n2. disjunction\n3. conditional\n4. biconditional\n"<<endl;
+	cout<<"0 for false 1 for true"<<endl;
+	cin>>p>>q;
+	{
+	if(p==1&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==1&&q==0)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==0)
+	{
+		cout<<"statement is FALSE"<<endl;
+	}
+	{
+	if(p==1&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==1&&q==0)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==0)
+	{
+		cout<<"statement is FALSE"<<endl;
+	}
+	}
+	{
+	if(p==1&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==1&&q==0)
+	{
+		cout<<"statement is FALSE"<<endl;
+	}
+	if(p==0&&q==0)
+	{
+		cout<<"statement is TRUE"<<endl;	
+	}
+	}
+	if(p==1&&q==1)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	if(p==0&&q==1)
+	{
+		cout<<"statement is FALSE"<<endl;
+	}
+	if(p==1&&q==0)
+	{
+		cout<<"statement is FALSE"<<endl;
+	}
+	if(p==0&&q==0)
+	{
+		cout<<"statement is TRUE"<<endl;
+	}
+	}	
+	return 0;
+}
+
+	
